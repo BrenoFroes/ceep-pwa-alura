f49a3e1e8bb547793bd8100c6e66c4096e588115
Breno Alves Fróes Peres <breno.froes@contabilizei.com.br>
2023-01-11 11:00:25 -0300

    feat: segundo capitulo completo

--- js/login/LoginUsuario.js
+++ js/login/LoginUsuario.js
@@ -1,1 +1,16 @@
-let logado = false;
+let logado = JSON.parse(localStorage.getItem("logado"))
+
+LoginUsuario_render({
+    logado: logado
+    ,usuario: localStorage.getItem("nomeUsuario")
+    ,onLogin: (nomeUsuario) => {
+        logado = true
+        localStorage.setItem("logado", true)
+        localStorage.setItem("nomeUsuario", nomeUsuario)
+    }
+    ,onLogout: () => {
+        logado = false
+        localStorage.setItem("logado", false)
+        localStorage.removeItem("nomeUsuario")
+    }
+})
